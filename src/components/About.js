f12bac5efc7fba18e1302e9785a7ccaf296c2ebc
Vishal Patel <patelvishalkumarvinayakprasad@gmail.com>
2022-03-01 23:50:07 +0530

    Shop page is created

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,23 @@ function About(){
   return(<>
   <div className="about">
  <div className="container">
+ <div className="row">
+   <div className="col-6">
    <h3>About us</h3>
-   <h1>Welcome to Maestro Pizzini</h1>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque temporibus magnam tempore architecto. Labore itaque repellat corrupti aliquid expedita facere?</p>
-   <div className="about_btn">
-     <a href="/" className='btn btn_smart'>READ MORE</a>
+   <h1>Welcome to <br /> Maestro Pizzini</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Itaque temporibus magnam tempore architecto.  Labore itaque repellat corrupti aliquid expedita <br /> facere?</p>
+
+   <div className="btn about_btn">
+     <a href="/" className='btn '>READ MORE</a>
    </div>
+   </div>
+   <div className="col-6">
+    <div className="about_img">
+      <img src="/Images/pizza5.jpg" alt="No Image found" />
+    </div>
+   </div>
+ </div>
+   
  </div>
  </div>
   </>
